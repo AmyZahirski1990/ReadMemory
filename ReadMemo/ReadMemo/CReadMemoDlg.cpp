61464adf366f043efdb327bb350e8dcb746290c6
hananiahhsu <hananiahhsu@126.com>
2018-10-20 12:06:44 +0800

    add dlg

---
+++ ReadMemo/ReadMemo/CReadMemoDlg.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "CReadMemoDlg.h"
+
+
+CReadMemoDlg::CReadMemoDlg()
+{
+}
+
+
+CReadMemoDlg::~CReadMemoDlg()
+{
+}
---
+++ WriteMemo/CWriteMemoDlg.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "CWriteMemoDlg.h"
+
+
+CWriteMemoDlg::CWriteMemoDlg()
+{
+}
+
+
+CWriteMemoDlg::~CWriteMemoDlg()
+{
+}
